474479d31d0408d2655fde967d279453d673c30a
VictorMello1993 <victorsmello93@gmail.com>
2023-01-16 23:21:54 -0300

    Front-end setup

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import { Habit } from "./components/Habit"
+import './styles/global.css'
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+    </div>
+  )
+}
+
+export default App
