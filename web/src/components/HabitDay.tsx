1a063a0ce445c8e7d9c0797903ce2c2c2dfaf71e
VictorMello1993 <victorsmello93@gmail.com>
2023-01-21 00:00:22 -0300

    Posting habits from client to server

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -3,15 +3,20 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 import { Check } from 'phosphor-react';
+import dayjs from 'dayjs';
 
 interface HabitDayProps {
-  date?: Date,
+  date: Date,
   availableHabits?: number,
   completedHabits?: number
 }
 
-export function HabitDay({ availableHabits = 0, completedHabits = 0 }: HabitDayProps) {
+export function HabitDay({ availableHabits = 0, completedHabits = 0, date }: HabitDayProps) {
   const completedPercentage = availableHabits > 0 ? Math.round((completedHabits / availableHabits) * 100) : 0
+
+  const dayAndMonth = dayjs(date).format('DD/MM')
+  const dayOfWeek = dayjs(date).format('dddd')
+
   return (
     <Popover.Root>
       <Popover.Trigger
@@ -25,8 +30,8 @@ export function HabitDay({ availableHabits = 0, completedHabits = 0 }: HabitDayP
         })} />
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-          <span className='font-semibold text-zinc-400'>terça-feira</span>
-          <span className='mt-1 font-extrabold leading-tight text-3xl'>20/01</span>
+          <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
+          <span className='mt-1 font-extrabold leading-tight text-3xl'>{dayAndMonth}</span>
           <ProgressBar progress={completedPercentage} />
           <div className='mt-6 flex flex-col gap-3'>
             <Checkbox.Root className='flex items-center gap-3 group'>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import { Check } from "phosphor-react";
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { FormEvent, FormEventHandler, useState } from "react";
+import { api } from "../libs/axios";
 
 const availableWeekDays = [
   'Domingo',
@@ -16,8 +17,24 @@ export function NewHabitForm() {
   const [title, setTitle] = useState('')
   const [weekDays, setWeekDays] = useState<number[]>([])
 
-  function createNewHabit(event: FormEvent) {
+  async function createNewHabit(event: FormEvent) {
     event.preventDefault()
+
+    if (!title || weekDays.length === 0) {
+      return
+    }
+
+    console.log(weekDays)
+
+    await api.post('habits', {
+      title,
+      weekDays
+    })
+
+    setTitle('');
+    setWeekDays([]);
+
+    alert('Hábito criado com sucesso!');
   }
 
   function handleToggleWeekDay(weekDay: number) {
@@ -38,6 +55,7 @@ export function NewHabitForm() {
       </label>
       <input type="text"
         id="title"
+        value={title}
         placeholder="ex.: Exercícios, dormir bem, etc..." autoFocus
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         onChange={(event) => setTitle(event.target.value)}
@@ -53,6 +71,7 @@ export function NewHabitForm() {
             <Checkbox.Root
               className='flex items-center gap-3 group'
               key={weekDay}
+              checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
               <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border 2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
