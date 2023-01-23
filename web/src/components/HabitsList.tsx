b7ede3039516687d415ef2d5103478987e185f4c
VictorMello1993 <victorsmello93@gmail.com>
2023-01-22 21:47:22 -0300

    scroll navigation enabled in habit list

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -3,6 +3,7 @@ import dayjs from "dayjs";
 import { Check } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { api } from "../libs/axios";
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 
 interface HabitsListProps {
   date: Date,
@@ -70,13 +71,13 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
   const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
 
   return (
-    <div className='mt-6 flex flex-col gap-3'>
+    <div className='mt-6 flex flex-col gap-3 max-h-72 overflow-y-auto'>
       {habitsInfo?.possibleHabits.map(habit => {
         return (
           <Checkbox.Root
             key={habit.id}
             onCheckedChange={() => handleToggleHabit(habit.id)}
-            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
+            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed ml-4 mt-2'
             checked={habitsInfo.completedHabits.includes(habit.id)}
             disabled={isDateInPast}>
             <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border 2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'>
