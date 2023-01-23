e410db85bd6dad9d3f2cedeb4cc6fb5137ddb7f6
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-01-23 00:14:59 -0300

    Add custom scrollbar

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -5,91 +5,96 @@ import { useEffect, useState } from "react";
 import { api } from "../libs/axios";
 
 interface HabitsListProps {
-  date: Date,
-  onCompletedChanged: (habitsCompleted: number) => void,
+  date: Date;
+  onCompletedChanged: (habitsCompleted: number) => void;
 }
 
 interface HabitsInfo {
   possibleHabits: Array<{
-    id: string
-    title: string
-    created_at: string
-  }>
+    id: string;
+    title: string;
+    created_at: string;
+  }>;
 
-  completedHabits: string[]
+  completedHabits: string[];
 }
 
 interface DayResponse {
   possibleHabits: Array<{
-    id: string
-    title: string
-    created_at: string
-  }>
+    id: string;
+    title: string;
+    created_at: string;
+  }>;
 
-  completedHabits: string[]
+  completedHabits: string[];
 }
 
-export function HabitsList({ date, onCompletedChanged, ...props }: HabitsListProps) {
-  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
+export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
 
   async function handleToggleHabit(habitId: string) {
-    await api.patch(`/habits/${habitId}/toggle`)
+    await api.patch(`/habits/${habitId}/toggle`);
 
-    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+    const isHabitAlreadyCompleted =
+      habitsInfo!.completedHabits.includes(habitId);
 
-    let completedHabits: string[] = []
+    let completedHabits: string[] = [];
 
     if (isHabitAlreadyCompleted) {
-      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
-    }
-    else {
-      completedHabits = [...habitsInfo!.completedHabits, habitId]
+      completedHabits = habitsInfo!.completedHabits.filter(
+        (id) => id !== habitId
+      );
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
     }
 
     setHabitsInfo({
       possibleHabits: habitsInfo!.possibleHabits,
-      completedHabits
-    })
+      completedHabits,
+    });
 
     onCompletedChanged(completedHabits.length);
   }
 
   useEffect(() => {
-    api.get('day', {
-      params: {
-        date: date.toISOString()
-      }
-    }).then(response => {
-      const result = response.data as DayResponse
+    api
+      .get("day", {
+        params: {
+          date: date.toISOString(),
+        },
+      })
+      .then((response) => {
+        const result = response.data as DayResponse;
 
-      onCompletedChanged(result.completedHabits.length);
-      setHabitsInfo(result as HabitsInfo)
-    })
-  }, [])
+        onCompletedChanged(result.completedHabits.length);
+        setHabitsInfo(result as HabitsInfo);
+      });
+  }, []);
 
-  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
+  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
 
   return (
-    <div className='mt-6 flex flex-col gap-3 max-h-72' {...props}>
-      {habitsInfo?.possibleHabits.map(habit => {
+    <div className="mt-6 flex flex-col gap-3 max-h-20">
+      {habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root
             key={habit.id}
             onCheckedChange={() => handleToggleHabit(habit.id)}
-            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed ml-4 mt-2'
+            className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed ml-4 mt-2"
             checked={habitsInfo.completedHabits.includes(habit.id)}
-            disabled={isDateInPast}>
-            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border 2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'>
+            disabled={isDateInPast}
+          >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border 2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background">
               <Checkbox.Indicator>
-                <Check size={20} className='text-white' />
+                <Check size={20} className="text-white" />
               </Checkbox.Indicator>
             </div>
-            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
               {habit.title}
             </span>
           </Checkbox.Root>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
