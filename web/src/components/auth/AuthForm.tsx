a3a08bc8672f9555a563c1a0f64d97db689d64d1
VictorMello1993 <victorsmello93@gmail.com>
2023-02-26 23:30:56 -0300

    feat: signup and signin form (part 2)

--- web/src/components/auth/AuthForm.tsx
+++ web/src/components/auth/AuthForm.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-
 export const texts = {
   alreadyHasAccount: 'Já tem uma conta? Faça login',
   NoAccount: 'Ainda não tem uma conta? Inscreva-se',
@@ -11,7 +8,14 @@ export const texts = {
   Signin: 'Entrar'
 }
 
-export function AuthForm() {
+interface ISignupFormProps {
+  children?: React.ReactNode
+}
+
+export function AuthForm({ children }: ISignupFormProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <span className="text-3l leading-tight font-semibold">Por favor, digite suas informações de cadastro</span>
@@ -36,12 +40,6 @@ export function AuthForm() {
             onChange={(event) => { setPassword(event.target.value) }}
           />
         </div>
-        {/* <button type="submit" className="mt-6 rounded-lg p-4 w-full flex items-center justify-center gap-3 font-semibold bg-violet-500 hover:bg-violet-700 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
-        Criar conta
-      </button> */}
-        {/* <span className="inline-block mt-4">
-        Já possui a sua conta? <Link to="/signin" className="hover:text-violet-300 transition-colors">Faça login</Link>
-      </span> */}
       </form >
     </div >
   )
