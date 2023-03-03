8b6ddeebe84d21389d515ca0ed45b45c2d3d3f57
VictorMello1993 <victorsmello93@gmail.com>
2023-03-02 22:58:47 -0300

    feat: password strength (part1)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -153,6 +153,16 @@ export async function appRoutes(app: FastifyInstance) {
       password: z.string()
     })
 
+    //Implementar a lógica da força da senha
+    //Possível regex:
+    //^                               start anchor
+    /*(?=(.*[a-z]){3,})               lowercase letters. {3,} indicates that you want 3 of this group
+    (?=(.*[A-Z]){2,})               uppercase letters. {2,} indicates that you want 2 of this group
+    (?=(.*[0-9]){2,})               numbers. {2,} indicates that you want 2 of this group
+    (?=(.*[!@#$%^&*()\-__+.]){1,})  all the special characters in the [] fields. The ones used by regex are escaped by using the \ or the character itself. {1,} is redundant, but good practice, in case you change that to more than 1 in the future. Also keeps all the groups consistent
+    {8,}                            indicates that you want 8 or more
+    $ */
+
     const { email, password } = createUserBody.parse(request.body);
 
     const hashedPassword = createHash('sha256').update(password).digest('base64');
